2de0b70b67f9afc0812b61244939c69215d36d75
Robocoders <team@blackbox.ai>
2024-10-10 22:48:24 +0000

    Created view-voter.tsx and view-all-voter.tsx in pages/components/

---
+++ pages/components/view-all-voter.tsx
@@ -0,0 +1,10 @@
+
+import ViewAllVoterPage from "@/components/ViewAllVoterPage";
+
+export default function ViewAllVoterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
+      <ViewAllVoter />
+    </div>
+  );
+}
