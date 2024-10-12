62b29664d1312b8514a62c4b9007897441c9a433
Robocoders <team@blackbox.ai>
2024-10-12 12:53:02 +0000

    Updated ViewVoterPage to remove additional classnames

--- pages/components/view-voter.tsx
+++ pages/components/view-voter.tsx
@@ -3,7 +3,7 @@ import ViewVoter from "@/components/ViewVoter";
 
 export default function ViewVoterPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
+    <div >
       <ViewVoter />
     </div>
   );
