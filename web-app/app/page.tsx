7115cec0f5cf9323f683473a0174063d214514c4
bc-rep-project <johannderbiel@gmail.com>
2024-10-14 02:21:23 +0000

    Remove redundant section closing tag from app/page.tsx

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <div className="flex-1 p-4">
         <section id="home">
           <HomePage />
-        </section>
-
+        <section id="home">
+          <HomePage />
         </section>
       </div>
     </div>
